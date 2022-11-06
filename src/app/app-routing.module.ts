a4c9573636987352f513fe89cfe36d7b484006ff
Michael Strauß <strauss.michael@gmx.net>
2022-11-06 18:22:58 +0100

    integrate channel and chat main page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { SignUpComponent } from './authentication/sign-up/sign-up.component';
 import { VerifyEmailComponent } from './authentication/verify-email/verify-email.component';
 import { MainComponent } from './main/main.component';
 import { PersonalComponent } from './main/personal/personal.component';
+import { ChannelComponent } from './main/main-page/channel/channel.component';
 
 const routes: Routes = [
   //{ path: '', redirectTo: '/sign-in', pathMatch: 'full' },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'verify-email-address', component: VerifyEmailComponent },
   { path: 'personal', component: PersonalComponent },
+  { path: 'channel', component: ChannelComponent },
 ];
 
 @NgModule({
