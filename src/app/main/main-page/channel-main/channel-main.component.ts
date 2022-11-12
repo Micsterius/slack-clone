135c8d2a4e79aacc81acbf5ccbb934d8cb2e24da
Michael Strauß <strauss.michael@gmx.net>
2022-11-12 05:53:48 +0100

    add commentar in channel

--- src/app/main/main-page/channel-main/channel-main.component.ts
+++ src/app/main/main-page/channel-main/channel-main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { initializeApp } from 'firebase/app';
-import { collection, getFirestore, onSnapshot, query } from 'firebase/firestore';
+import { User } from 'firebase/auth';
+import { collection, doc, getFirestore, onSnapshot, query, setDoc } from 'firebase/firestore';
 import { ChannelService } from 'src/app/shared/services/channel.service';
 import { DetailViewPageService } from 'src/app/shared/services/detail-view-page.service';
 import { environment } from 'src/environments/environment';
@@ -19,10 +20,14 @@ export class ChannelMainComponent implements OnInit {
   posts: any[] = [];
   showChannel: boolean = true;
   currentChannelId: string = '';
+  message: any;
+  actualUser: User;
+
   constructor(
     public channelServ: ChannelService,
     public detailViewService: DetailViewPageService) {
     this.currentChannel = JSON.parse(localStorage.getItem('currentChannel')!)
+    this.actualUser = JSON.parse(localStorage.getItem('user')!)
     channelServ.currentChannel = this.currentChannel
   }
 
@@ -30,7 +35,7 @@ export class ChannelMainComponent implements OnInit {
   }
 
 
-  
+
 
   changeDetailViewPageContentToThread() {
     this.detailViewService.showUserInfo = false;
@@ -45,5 +50,31 @@ export class ChannelMainComponent implements OnInit {
     localStorage.setItem('answersForThread', JSON.stringify(answersForThread));
     this.channelServ.showThread = true; //proof if it is necessary, two times called
     this.channelServ.currentThread = answersForThread;
+  }
+
+  /**here the new doc id in the subcollection texts will be generated with two components. 
+ * The first one is a timestamp, so the messeages are in the right order when they come 
+ * from firestore. The second component is a randowm string with 6 characters if two 
+ * users post at the same time. */
+  async sendMessage() {
+    let textId = Math.round(new Date().getTime() / 1000);
+    let idAdd = Math.random().toString(16).substr(2, 6)
+
+    let name = this.getNameOfAuthor();
+    
+    await setDoc(doc(this.db, "channel", this.channelServ.currentChannel.id, "posts", `${textId + idAdd}`),
+      {
+        content: this.message,
+        authorId: this.actualUser.uid,
+        authorName: name,
+        id: `${textId + idAdd}`,
+        timeStamp: textId
+      })
+    this.message = '';
+  }
+
+  getNameOfAuthor(){
+    if (this.actualUser.displayName) return this.actualUser.displayName;
+    else return 'Anonym'
   }
 }
