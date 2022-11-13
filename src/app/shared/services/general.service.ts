9f4877e24aa3818838e4a3f6a6f4c26a1c251f7e
Michael Strauß <strauss.michael@gmx.net>
2022-11-13 13:48:50 +0100

    fixed time stamp bug in general service

--- src/app/shared/services/general.service.ts
+++ src/app/shared/services/general.service.ts
@@ -9,7 +9,7 @@ export class GeneralService {
 
   getTransformedTimeStampToDate(timeStamp) {
     let time = Number(timeStamp) 
-    let date = new Date(Number(time));
+    let date = new Date(time * 1000);
     let cDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
     let cTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
     let dateTime = cDate + ' ' + cTime;
