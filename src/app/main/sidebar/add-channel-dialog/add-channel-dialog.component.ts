9d3b8fa78260216b4064c418ae07d725e2cd4b21
Tuncay Dagdelen <sramos083@gmail.com>
2022-11-14 13:35:09 +0100

    create channel works

--- src/app/main/sidebar/add-channel-dialog/add-channel-dialog.component.ts
+++ src/app/main/sidebar/add-channel-dialog/add-channel-dialog.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { initializeApp } from 'firebase/app';
+import { collection, getDocs, getFirestore, onSnapshot, query, addDoc, doc, updateDoc } from 'firebase/firestore';
+import { ChannelService } from 'src/app/shared/services/channel.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-add-channel-dialog',
@@ -7,11 +11,27 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./add-channel-dialog.component.scss']
 })
 export class AddChannelDialogComponent implements OnInit {
-  name: any;
+  app = initializeApp(environment.firebase);
+  db = getFirestore(this.app);
+  @Input() name:any;
 
-  constructor(public dialogRef: MatDialogRef<AddChannelDialogComponent>) { }
+  constructor(
+    public dialogRef: MatDialogRef<AddChannelDialogComponent>,
+    public channel: ChannelService
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  async createNewChannel(){
+    if(this.name){
+      let docRef = await addDoc(collection(this.db, "channel"), {
+        id: 'id', //push doc id here :)
+        name: this.name, 
+      });
+    } else{
+      alert('Bitte Channel mit mindestens 3 zeichen eigeben')
+    }
   }
 
 }
