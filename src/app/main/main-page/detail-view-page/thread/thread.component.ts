cca5c2924e8ce8c2aa07f4e83122006e75406c49
Michael Strauß <strauss.michael@gmx.net>
2022-11-14 18:40:41 +0100

    implement post in thread

--- src/app/main/main-page/detail-view-page/thread/thread.component.ts
+++ src/app/main/main-page/detail-view-page/thread/thread.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChannelService } from 'src/app/shared/services/channel.service';
+import { GeneralService } from 'src/app/shared/services/general.service';
+import { UsersService } from 'src/app/shared/services/users.service';
 
 @Component({
   selector: 'app-thread',
@@ -11,7 +13,10 @@ export class ThreadComponent implements OnInit {
   answers: any [] = []
   message: any;
   constructor(
-    public channelServ: ChannelService
+    public channelServ: ChannelService,
+    public usersService: UsersService,
+    public generalService: GeneralService,
+
   ) {
   /*  this.answersForThread = JSON.parse(localStorage.getItem('answersForThread')!);
     this.answers = this.answersForThread.answers;*/
