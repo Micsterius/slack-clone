35d8b6453181754bebc18476a39e8938c6e4201d
Tuncay Dagdelen <sramos083@gmail.com>
2022-12-07 15:46:56 +0100

    user preview in add chat fixed

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -52,7 +52,7 @@ export class AuthService {
         JSON.parse(localStorage.getItem('user')!);
       }
     });
-    console.log(this.userData)
+    // console.log(this.userData)
   }
 
   // Sign in with email/password
