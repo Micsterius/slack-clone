b60cb1d42600621e93ea5cabc4dedd74477ec65c
Michael Strauß <strauss.michael@gmx.net>
2022-12-13 11:35:14 +0100

    implement close profile view option

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,6 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { doc, getFirestore, updateDoc } from 'firebase/firestore';
 import { environment } from 'src/environments/environment';
 import { AuthService } from '../shared/services/auth.service';
+import { DetailViewPageService } from '../shared/services/detail-view-page.service';
 import { UsersService } from '../shared/services/users.service';
 
 @Component({
@@ -17,25 +18,24 @@ export class MainComponent implements OnInit {
   storageTime;
   time = 0;
 
-  pageReloaded = window.performance
-    .getEntriesByType('navigation')
-    .map((nav) => (nav as any).type)
-    .includes('reload');
-
   constructor(
     private authService: AuthService,
-    private userService: UsersService
+    private userService: UsersService,
+    public detailService: DetailViewPageService
   ) {
     this.activeUser = JSON.parse(localStorage.getItem('user')!);
 
-     document.addEventListener('visibilitychange', () => {
-       if (document.hidden) this.userIsAway()
-       else this.userIsStillActive()
-     })
- 
-     document.addEventListener('touchstart', () => {
-       this.userIsStillActive()
-     })
+    document.addEventListener('visibilitychange', () => {
+      if (document.hidden) this.userIsAway()
+      else this.userIsStillActive()
+    })
+
+    document.addEventListener('touchstart', () => {
+      this.userIsStillActive()
+    })
+  }
+
+  ngOnInit(): void {
   }
 
   async userIsStillActive() {
@@ -51,9 +51,7 @@ export class MainComponent implements OnInit {
       }
     }
   }
-  
-  ngOnInit(): void {
-  }
+
 
   async userIsAway() {
     if (await this.userService.UserDataOfUserExist(this.activeUser.uid)) {
@@ -65,5 +63,11 @@ export class MainComponent implements OnInit {
         });
       }
     }
+  }
+
+  closeDetailView() {
+    this.detailService.showOtherUserInfo = false;
+    this.detailService.showThread = false;
+    this.detailService.showUserInfo = false;
   }
 }
