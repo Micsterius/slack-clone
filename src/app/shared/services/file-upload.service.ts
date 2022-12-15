7c90596f96d42515bdb9216945bb5dfdde34f40f
Michael Strauß <strauss.michael@gmx.net>
2022-12-15 12:50:51 +0100

    upload service change with progress console.log

--- src/app/shared/services/file-upload.service.ts
+++ src/app/shared/services/file-upload.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { getDownloadURL, getStorage, ref, uploadBytes, uploadBytesResumable } from 'firebase/storage';
 import { finalize, Observable } from 'rxjs';
 import { FileUpload } from 'src/app/models/file-upload.model';
 
@@ -8,13 +9,14 @@ import { FileUpload } from 'src/app/models/file-upload.model';
   providedIn: 'root'
 })
 export class FileUploadService {
+  storageFB = getStorage();
   private basePath = '/uploads';
   public basePathUser: string = '';
   constructor(
     private db: AngularFireDatabase,
     private storage: AngularFireStorage) { }
 
-  pushFileToStorage(fileUpload: FileUpload): Observable<number | undefined> {
+/*  pushFileToStorage(fileUpload: FileUpload): Observable<number | undefined> {
     const filePath = `${this.basePath}/${fileUpload.file.name}`;
     const storageRef = this.storage.ref(filePath);
     const uploadTask = this.storage.upload(filePath, fileUpload.file);
@@ -30,7 +32,7 @@ export class FileUploadService {
 
     return uploadTask.percentageChanges();
   }
-
+*/
   pushUserImageFileToStorage(fileUpload: FileUpload): Observable<number | undefined> {
     const filePath = `${this.basePathUser}/${fileUpload.file.name}`;
     const storageRef = this.storage.ref(filePath);
@@ -48,29 +50,40 @@ export class FileUploadService {
     return uploadTask.percentageChanges();
   }
 
-  private saveFileData(fileUpload: FileUpload): void {
-    this.db.list(this.basePath).push(fileUpload);
+  pushFileToStorage(fileUpload: FileUpload){
+    const filePath = `${this.basePath}/${fileUpload.file.name}`;
+    const storageRef = ref(this.storageFB, filePath);
+    const uploadTask = uploadBytesResumable(storageRef, fileUpload.file);
+    uploadBytes(storageRef, fileUpload.file).then((snapshot) => {
+      console.log('Uploaded a blob or file!');
+    });
+
+    uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+    }
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      return true;
+    });
+  }
+);
   }
 
-  getFiles(numberItems: number): AngularFireList<FileUpload> {
-    return this.db.list(this.basePath, ref =>
-      ref.limitToLast(numberItems));
-  }
-
-  deleteFile(fileUpload: FileUpload): void {
-    this.deleteFileDatabase(fileUpload.key)
-      .then(() => {
-        this.deleteFileStorage(fileUpload.name);
-      })
-      .catch();
-  }
-
-  private deleteFileDatabase(key: string): Promise<void> {
-    return this.db.list(this.basePath).remove(key);
-  }
-
-  private deleteFileStorage(name: string): void {
-    const storageRef = this.storage.ref(this.basePath);
-    storageRef.child(name).delete();
-  }
 }
